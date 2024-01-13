c46ac22c4162d93a8b3c0612bde8fb1d7da06795
Adebisea <abdulgkhadijah@gmail.com>
2024-01-13 18:46:41 +0100

    added the az datasourse

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,16 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block       = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block            = "10.0.0.0/16"
+  enable_dns_hostnames  = true
 
-  tags = {
+  tags   = {
     Name = "dev_vpc"
   }
 }
+
+data "aws_availability_zones" "az_list" {
+  state = "available"
+}
+
+
